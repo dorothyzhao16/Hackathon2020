36cf9381473e6175dfa13d793fbd8777c60294a3
JasonShadle <jason.shadle50@gmail.com>
2020-01-03 19:40:42 -0500

    console log

--- app.js
+++ app.js
@@ -8,10 +8,17 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+    let authorMention = msg.author;
+    let message = msg.content;
+    let authorID = authorMention.id;
+    let channel = msg.channel;
+    let authorName = authorMention.username;
+    console.log(`${authorName}: ${message}`);
     // don't respond to another bot message
     if (msg.author.bot) {
         return
     }
+    msg.channel.send(`${authorMention}: hi :)`);
 });
 
 // log you in
