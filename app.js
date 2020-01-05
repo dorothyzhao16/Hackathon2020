366bfae45732a95a6555e3d893f1cba9d69b37af
JasonShadle <jason.shadle50@gmail.com>
2020-01-05 18:19:13 -0500

    Final update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const SequelizeConnect = new Sequelize({
     host: 'localhost',
     dialect: 'mysql',
     dialectOptions: {
-      charset: 'utf8mb4'
+        charset: 'utf8mb4'
     },
     pool: {
         max: 20,
@@ -16,9 +16,9 @@ const SequelizeConnect = new Sequelize({
         idle: 10000
     },
     define: {
-      charset: 'utf8',
-      collate: 'utf8_general_ci',
-      timestamps: true
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+        timestamps: true
     },
     logging: false
 });
@@ -77,120 +77,108 @@ client.on('message', msg => {
                 }
             })
             // console.log(roles);
-        }
-        else if (args[1] == 'school') { // Cassidy's working on it!
+        } else if (args[1] == 'school') {
             //PM user link to correct school server
 
-		if(args[2] == 'OSU'){
-    			const m = `https://discord.gg/6wJk2VB`;
-    			msg.member.send(m);
+            if (args[2] == 'OSU') {
 
-				// user has the role
-                    		if (msg.member.roles.has('662821729017790465')) {
-                       			 msg.member.removeRole('662821729017790465');
-					 msg.channel.send(`${authorMention}: Your school, Ohio State, has been removed.`);
-                    		}
+                // user has the role
+                if (msg.member.roles.has('662821729017790465')) {
+                    msg.member.removeRole('662821729017790465');
+                    msg.channel.send(`${authorMention}: Your school, Ohio State, has been removed.`);
+                }
 
-                    		// user doesn't have the role
-                    		else {
-				msg.channel.send(`${authorMention}: Your school, Ohio State, has been added.`);
-                        	msg.member.addRole('662821729017790465');
+                // user doesn't have the role
+                else {
+                    msg.channel.send(`${authorMention}: Your school, Ohio State, has been added.`);
+                    msg.member.addRole('662821729017790465');
 
-    				const m = `https://discord.gg/6wJk2VB`;
-    				msg.member.send(m);
-                    		}
+                    const m = `https://discord.gg/6wJk2VB`;
+                    msg.member.send(m);
+                }
 
 
-		}
-		else{
+            } else {
 
-				// user has the role
-                    		if (msg.member.roles.has('662821589879881748')) {
-                       			 msg.member.removeRole('662821589879881748');
-					 msg.channel.send(`${authorMention}: Your school, Cleveland State, has been removed.`);
-                    		}
+                // user has the role
+                if (msg.member.roles.has('662821589879881748')) {
+                    msg.member.removeRole('662821589879881748');
+                    msg.channel.send(`${authorMention}: Your school, Cleveland State, has been removed.`);
+                }
 
-                    		// user doesn't have the role
-                    		else {
-                        	msg.member.addRole('662821589879881748');
-				msg.channel.send(`${authorMention}: Your school, Cleveland State, has been added.`);
+                // user doesn't have the role
+                else {
+                    msg.member.addRole('662821589879881748');
+                    msg.channel.send(`${authorMention}: Your school, Cleveland State, has been added.`);
 
-    				const m = `https://discord.gg/Ej2Ftd4`;
-    				msg.member.send(m);
+                    const m = `https://discord.gg/Ej2Ftd4`;
+                    msg.member.send(m);
 
-                    		}
-	    	}
+                }
+            }
 
 
-        }
-        else if (args[1] == 'schools') {
+        } else if (args[1] == 'schools') {
             // display all available schools
             msg.channel.send(`Here's a list of schools! \nCSU - Cleveland State University\nOSU - Ohio State University`);
-        }
-        else if (args[1] == 'help') {
+        } else if (args[1] == 'help') {
             let embed = new Discord.RichEmbed()
                 .setTitle('CampusConnect Help')
-                .addField('Add or remove the role for your major','!cc major [major]')
-                .addField('Add or remove the role for your school','!cc school [school]')
-                .addField('See list of schools','!cc schools')
-                .addField('See number of points','!cc points');
-            msg.channel.send(`${authorMention}:`, embed=embed);
-        }
-        else if (args[1] == 'points') {
+                .addField('Add or remove the role for your major', '!cc major [major]')
+                .addField('Add or remove the role for your school', '!cc school [school]')
+                .addField('See list of schools', '!cc schools')
+                .addField('See number of points', '!cc points');
+            msg.channel.send(`${authorMention}:`, embed = embed);
+        } else if (args[1] == 'points') {
             if (msg.mentions.members.first() != undefined) {
                 let mentionID = msg.mentions.members.first().user.id;
                 db.getUserPoints(mentionID)
-                .then(points => {
-                    msg.channel.send(`${authorMention}: <@${mentionID}> has ${points} points`);
-                })
-                .catch(console.error);
-            }
-            else {
+                    .then(points => {
+                        msg.channel.send(`${authorMention}: <@${mentionID}> has ${points} points`);
+                    })
+                    .catch(console.error);
+            } else {
                 db.getUserPoints(authorID)
-                .then(points => {
-                    msg.channel.send(`${authorMention}: You have ${points} points`);
-                })
-                .catch(console.error);
+                    .then(points => {
+                        msg.channel.send(`${authorMention}: You have ${points} points`);
+                    })
+                    .catch(console.error);
             }
         }
     }
 });
 
 client.on('guildMemberAdd', member => {
-    const m = `Welcome to the CampusConnects Discord Server!\n\nMake sure to add your roles and college in the <#662845246006362114> to gain permission!`;
+    const m = `Welcome to the CampusConnects Discord Server!\n\nMake sure to add your roles and college in the <#662845246006362114> to gain permission! Use \`!cc help\` to view the commands!`;
     member.send(m);
 })
 
 client.on('messageReactionAdd', (reaction, user) => {
     if (reaction.emoji.name === medalEmoji) {
         db.setUserPoints(reaction.message.author.id, 1)
-        .then(_ => {
-            db.getUserPoints(reaction.message.author.id)
-            .then(points => {
-
-                if (points == 10) {
-                    reaction.message.member.addRole('663093015539089435');
-                    reaction.message.channel.send(`Congrats <@${reaction.message.author.id}>! You have leveled up`);
-                }
-                else if (points == 25) {
-                    reaction.message.member.removeRole('663093015539089435');
-                    reaction.message.member.addRole('663093236725710859');
-                    reaction.message.channel.send(`Congrats <@${reaction.message.author.id}>! You have leveled up`);
-                }
-                else if (points == 50) {
-                    reaction.message.member.removeRole('663093236725710859');
-                    reaction.message.member.addRole('663093247031246858');
-                    reaction.message.channel.send(`Congrats <@${reaction.message.author.id}>! You have leveled up`);
-                }
-                else if (points == 100) {
-                    reaction.message.member.removeRole('663093247031246858')
-                    reaction.message.member.addRole('663093254136397854');
-                    reaction.message.channel.send(`Congrats <@${reaction.message.author.id}>! You have leveled up`);
-                }
+            .then(_ => {
+                db.getUserPoints(reaction.message.author.id)
+                    .then(points => {
+                        if (points == 10) {
+                            reaction.message.member.addRole('663093015539089435');
+                            reaction.message.channel.send(`Congrats <@${reaction.message.author.id}>! You have leveled up`);
+                        } else if (points == 25) {
+                            reaction.message.member.removeRole('663093015539089435');
+                            reaction.message.member.addRole('663093236725710859');
+                            reaction.message.channel.send(`Congrats <@${reaction.message.author.id}>! You have leveled up`);
+                        } else if (points == 50) {
+                            reaction.message.member.removeRole('663093236725710859');
+                            reaction.message.member.addRole('663093247031246858');
+                            reaction.message.channel.send(`Congrats <@${reaction.message.author.id}>! You have leveled up`);
+                        } else if (points == 100) {
+                            reaction.message.member.removeRole('663093247031246858')
+                            reaction.message.member.addRole('663093254136397854');
+                            reaction.message.channel.send(`Congrats <@${reaction.message.author.id}>! You have leveled up`);
+                        }
+                    })
+                    .catch(console.error);
             })
             .catch(console.error);
-        })
-        .catch(console.error);
     }
 })
 
